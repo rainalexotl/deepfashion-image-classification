8615f82f95904843aa2fec500759f1870e02f4c6
rainalexotl <rainalex01@gmail.com>
2025-04-29 23:51:46 -0400

    add alexnet

--- src/models/factory.py
+++ src/models/factory.py
@@ -1,3 +1,6 @@
+import torch.nn as nn
+
+from torchvision import models
 from src.models.baseline import BaselineCNN
 
 def get_model(config):
@@ -7,5 +10,21 @@ def get_model(config):
 
     if model_name == 'baseline':
         return BaselineCNN(num_classes)
+    elif model_name == 'alexnet':
+        AlexNetModel = models.alexnet(pretrained=pretrained)
+        for param in AlexNetModel.parameters():
+            param.requires_grad = False
+
+        AlexNetModel.classifier = nn.Sequential(
+            nn.Linear(9216, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+
+        return AlexNetModel
     else:
         raise ValueError(f"Unknown model: {model_name}")
