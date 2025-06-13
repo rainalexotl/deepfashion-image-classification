dee91746727ece092de472d7efb0c74b4fa9fcfb
rainalexotl <rainalex01@gmail.com>
2025-06-13 00:09:14 -0400

    migrate to ONNX for inference

--- serve/model.py
+++ serve/model.py
@@ -1,26 +1,29 @@
 import os
 from pathlib import Path
 
+import numpy as np
 import torch
 import torchvision.transforms.v2 as T
-from transformers import SwinForImageClassification, AutoConfig
 from huggingface_hub import hf_hub_download
+import onnxruntime as ort
 
 LABELS = ['Jumpsuit', 'Top', 'Bomber', 'Skirt', 'Tee', 'Sweatpants', 'Flannel', 
           'Tank', 'Turtleneck', 'Jersey', 'Blouse', 'Kaftan', 'Jeggings', 'Shirtdress', 
           'Sarong', 'Jacket', 'Leggings', 'Nightdress', 'Sweatshorts', 'Coverup']
 
-MODEL_PATH = Path("swin_best_model.pt")
+REPO_ID = 'rainalexotl/deepfashion-image-classification-swin'
+FILENAME = 'swin_model.onnx'
+HF_TOKEN = os.getenv('HF_TOKEN') 
+MODEL_PATH = Path(
+    hf_hub_download(
+        repo_id=REPO_ID,
+        filename=FILENAME,
+        token=HF_TOKEN
+    )
+)
 
 def load_model():
-    config = AutoConfig.from_pretrained('microsoft/swin-base-patch4-window7-224')
-    config.num_labels = len(LABELS)
-    
-    model = SwinForImageClassification(config)
-    state_dict = torch.load(MODEL_PATH)
-    model.load_state_dict(state_dict)
-    model.eval()
-    return model
+    return ort.InferenceSession(MODEL_PATH, providers=["CPUExecutionProvider"])
 
 def make_prediction(model, image):
     transform = T.Compose([
@@ -31,9 +34,9 @@ def make_prediction(model, image):
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     
-    image = transform(image)
+    image = transform(image).unsqueeze(0).numpy()
 
-    with torch.no_grad():
-        outputs = model(pixel_values=image.unsqueeze(0))
-    prediction = outputs.logits.argmax(dim=1).item()
-    return LABELS[prediction]
+    outputs = model.run(None, {"pixel_values": image})
+    logits = outputs[0]
+    pred_idx = np.argmax(logits, axis=1)[0]
+    return LABELS[pred_idx]
